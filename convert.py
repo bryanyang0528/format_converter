d39f81f1785826f630faaed551ae3a2c55e52660
Bryan Yang <bryan.yang@Bryans-MacBook-Pro.local>
2017-06-23 12:24:41 +0800

    init

---
+++ convert.py
@@ -0,0 +1,11 @@
+from converter import Converter
+
+
+def main():
+    ct = Converter("","")
+    print(ct.head())
+    print(ct.take(10))
+
+
+if __name__ == '__main__':
+   main()
---
+++ converter/converter.py
@@ -0,0 +1,59 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Provides a common test case base for Python Spark tests"""
+
+from .utils import add_pyspark_path, quiet_py4j
+from pyspark.context import SparkContext, SparkConf
+import os
+
+class Converter():
+
+    def __init__(self, input, output):
+        self.setUp()
+        self.df = self.sqlCtx.read.csv(input,header=True)
+        self.output = output
+
+    def getMaster(self):
+        return os.getenv('SPARK_MASTER')
+
+    def setUp(self):
+        """Setup a basic Spark context for testing"""
+        #conf = SparkConf().setMaster(self.getMaster())
+        self.sc = SparkContext(self.getMaster())
+        quiet_py4j()
+        try:
+            from pyspark.sql import SparkSession
+            self.sqlCtx = SparkSession.builder.getOrCreate()
+        except:
+            self.sqlCtx = SQLContext(self.sc)
+
+    def tearDown(self):
+        """
+        Tear down the basic panda spark test case. This stops the running
+        context and does a hack to prevent Akka rebinding on the same port.
+        """
+        self.sc.stop()
+        # To avoid Akka rebinding to the same port, since it doesn't unbind
+        # immediately on shutdown
+        self.sc._jvm.System.clearProperty("spark.driver.port")
+ 
+    def head(self):
+        return self.df.head
+
+    def take(self, n):
+        return self.df.take(n)
